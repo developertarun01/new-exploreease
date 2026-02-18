599ef81b1736f41653808727578c3ede2a2aa3a8
developertarun01 <tarunbusinessmail@gmail.com>
2026-02-18 14:50:22 +0530

    Google Tags

--- temp.py
+++ temp.py
@@ -6,3 +6,18 @@
 # Emirates
 # Lufthansa 
 # British Airways
+
+def below_75Per(attendance):
+    print("Student with attendence less than 75%:")
+    for name in attendance:
+        n = len(attendance[name])
+        attendance[name]
+
+
+attendance = {
+   "Aman": [1,1,0,1,1],
+   "Riya": [1,0,0,1,0],
+   "Karan": [1,1,1,1,1]
+}
+
+below_75Per(attendance)
