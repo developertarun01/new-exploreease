d12c748ecf4d26ad4d0639b3da9d0f2da40f30a4
developertarun01 <tarunbusinessmail@gmail.com>
2026-02-18 15:31:07 +0530

    4 more airlines

--- temp.py
+++ temp.py
@@ -1,18 +1,20 @@
-# singapore airlines
-# thai airways 
-# ita airways
-# jal airlines
-# Qantas
-# Emirates
-# Lufthansa 
-# British Airways
-
 def below_75Per(attendance):
     print("Student with attendence less than 75%:")
+    highest = 0
     for name in attendance:
         n = len(attendance[name])
-        attendance[name]
-
+        li = attendance[name]
+        present = 0
+        for val in li:
+            if val == 1:
+                present += 1
+        presentPercent = (present/n) * 100
+        if presentPercent < 75:
+            print(name)
+        if(presentPercent>highest):
+            highest = presentPercent
+    print("Highest percentage is", highest)
+        
 
 attendance = {
    "Aman": [1,1,0,1,1],
@@ -21,3 +23,5 @@ attendance = {
 }
 
 below_75Per(attendance)
+
+# list is suitable for storing attendance instead of tuple because list is a mutable data type which can be changed during execution, so if a student comes late then attendence of the student can be changed from 0 to 1
